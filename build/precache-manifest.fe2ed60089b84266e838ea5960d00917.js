b0b200deb759515504dff7a6399f4546e501799d
amol <clever.hacker4fun@gmail.com>
2020-02-09 08:51:37 +0530

    Initial commit

---
+++ build/precache-manifest.fe2ed60089b84266e838ea5960d00917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "075baf76ca18f81ee71d2a673ff64a9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa31ebb851cd70f22b4b",
+    "url": "/static/css/main.f09003de.chunk.css"
+  },
+  {
+    "revision": "900523bfd327aea853b9",
+    "url": "/static/js/2.8f2bf2f7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8f2bf2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa31ebb851cd70f22b4b",
+    "url": "/static/js/main.87803b30.chunk.js"
+  },
+  {
+    "revision": "a5878287add1a19af6a7",
+    "url": "/static/js/runtime-main.4c046e39.js"
+  }
+]);
